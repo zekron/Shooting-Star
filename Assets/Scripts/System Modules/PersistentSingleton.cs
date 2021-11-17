1899117eccc0cea1b93cf16547bd427a5df02834
Zikzik <34153457+zekron@users.noreply.github.com>
2021-11-17 14:58:44 +0800

    Update Singleton pattern
    懒汉式

--- Assets/Scripts/System Modules/PersistentSingleton.cs
+++ Assets/Scripts/System Modules/PersistentSingleton.cs
@@ -2,19 +2,56 @@ using UnityEngine;
 
 public class PersistentSingleton<T> : MonoBehaviour where T : Component
 {
-    public static T Instance { get; private set; }
+    //public static T Instance { get; private set; }
 
-    protected virtual void Awake()
+    //protected virtual void Awake()
+    //{
+    //    if (Instance == null)
+    //    {
+    //        Instance = this as T;
+    //    }
+    //    else if (Instance != this)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+
+    //    DontDestroyOnLoad(gameObject);
+    //}
+
+    public static T Instance
     {
-        if (Instance == null)
+        get
         {
-            Instance = this as T;
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
+            var objs = FindObjectsOfType<T>();
+            if (objs.Length > 1)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("不应该存在多个单例！");
+#endif
+                for (int i = 1; i < objs.Length; i++)
+                {
+                    Destroy(objs[i].gameObject);
+                }
+                return instance;
+            }
+            else if (objs.Length == 1)
+            {
+                instance = objs[0];
+            }
 
-        DontDestroyOnLoad(gameObject);
+            if (instance == null)
+            {
+                var singleton = new GameObject();
+                instance = singleton.AddComponent<T>();
+                singleton.name = $"Singleton {typeof(T)}";
+                DontDestroyOnLoad(singleton.gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(instance.gameObject);
+            }
+            return instance;
+        }
     }
+    private static T instance;
 }
--- Assets/Scripts/System Modules/Singleton.cs
+++ Assets/Scripts/System Modules/Singleton.cs
@@ -2,10 +2,36 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Component
 {
-    public static T Instance { get; private set; }
-
-    protected virtual void Awake()
+    public static T Instance
     {
-        Instance = this as T;
+        get
+        {
+            var objs = FindObjectsOfType<T>();
+            if (objs.Length > 1)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("不应该存在多个单例！");
+#endif
+                for (int i = 1; i < objs.Length; i++)
+                {
+                    Destroy(objs[i].gameObject);
+                }
+                return instance;
+            }
+            else if (objs.Length == 1)
+            {
+                instance = objs[0];
+            }
+
+            if (instance == null)
+            {
+                var singleton = new GameObject();
+                instance = singleton.AddComponent<T>();
+                singleton.name = $"Singleton {typeof(T)}";
+            }
+            //DontDestroyOnLoad(instance.gameObject);
+            return instance;
+        }
     }
+    private static T instance;
 }
